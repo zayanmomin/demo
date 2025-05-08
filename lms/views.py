43548a51bc46b16be21392bc9a91a36cc330d729
Mohammed Zayan Momin <zayanmomin@gmail.com>
2025-05-08 23:45:41 +0400

    Add viewsets

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Author, Publisher, Book, Review
+from .serializers import AuthorSerializer, PublisherSerializer, BookSerializer, ReviewSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    """ViewSet for viewing and editing Author instances"""
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class PublisherViewSet(viewsets.ModelViewSet):
+    """ViewSet for viewing and editing Publisher instances"""
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """ViewSet for viewing and editing Book instances with filtering"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """ViewSet for viewing and editing Review instances nested under Books"""
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        """Return reviews for a specific book"""
+        return Review.objects.filter(book_id=self.kwargs['book_pk'])
+
+    def perform_create(self, serializer):
+        """Save the review with the book from the URL"""
+        book = get_object_or_404(Book, pk=self.kwargs['book_pk'])
+        serializer.save(book=book)
